b64f95e3bd441840f8070542eb6511620f2fb637
Truong Van Thong <thongvanpro2002@gmail.com>
2024-06-03 18:15:47 +0700

    chore: Add Dockerfile and compose.yaml for containerization

---
+++ README.md
@@ -0,0 +1,94 @@
+# Gemini PDF Chatbot
+Tiếng Việt: Gemini PDF Chatbot là một ứng dụng dựa trên Streamlit cho phép người dùng trò chuyện với một mô hình AI trò chuyện được đào tạo trên tài liệu PDF. Chatbot trích xuất thông tin từ các tệp PDF được tải lên và trả lời các câu hỏi của người dùng dựa trên ngữ cảnh được cung cấp.
+
+## Các tính năng
+
+- **PDF Upload:** Người dùng có thể tải lên các tệp PDF để trích xuất thông tin.
+- **Text Extraction:** Trích xuất văn bản từ tệp PDF và tạo các vectơ nhúng.
+- **Conversational AI:** Mô hình AI trả lời các câu hỏi dựa trên ngữ cảnh từ tài liệu PDF.
+- **Chat Interface:** Giao diện trò chuyện cho phép người dùng tương tác với mô hình AI.
+
+## Hướng dẫn lấy API key từ Google
+<!-- https://aistudio.google.com/app/apikey -->
+1. Truy cập vào trang [Google AI Platform](https://aistudio.google.com/app/apikey).
+2. Nhấp vào "Create API Key" để tạo một API key mới.
+3. Sao chép API key và lưu trữ nó một cách an toàn.
+
+## Cài đặt và sử dụng với Docker
+
+Nếu bạn đã cài đặt docker, bạn có thể chạy ứng dụng bằng lệnh sau:
+
+- Bây giờ bạn lấy API key từ Google và đặt nó trong file `.env`.
+   ```.env
+   GOOGLE_API_KEY=your_api_key_here
+   ```
+
+```bash
+docker compose up --build
+```
+- Mở trình duyệt và truy cập vào địa chỉ `http://localhost:8501`.
+
+## Cài đặt và sử dụng với Python (Local)
+
+   **Lưu ý:** Dự án này yêu cầu Python 3.10 hoặc cao hơn.
+
+1. **Clone the Repository:**
+
+   ```bash
+   https://github.com/truongvanthong/Langchain-Gemini-Streamlit.git
+   ```
+
+2. **Cài đặt các thư viện:**
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+3. **Set up Google API Key:**
+   - Lấy API key từ Google và đặt nó trong file `.env`
+
+   ```bash
+   GOOGLE_API_KEY='your_api_key_here'
+   ```
+
+4. **Run the Application:**
+
+   ```bash
+   streamlit run main.py
+   ```
+
+5. **Mở trình duyệt và truy cập vào địa chỉ `http://localhost:8501`.**
+
+## Hướng dẫn sử dụng
+
+1. **Chat Interface:**
+   - Giao diện chính cho phép người dùng trò chuyện với AI.
+
+2. **Upload PDFs:**
+   - Sử dụng thanh bên để tải lên các tệp PDF.
+   - Nhấp vào "Submit & Process" để trích xuất văn bản và tạo các vectơ nhúng.
+
+## Cấu trúc dự án
+
+- `app.py`: File chính chứa mã nguồn cho ứng dụng Streamlit.
+- `.env`: file chứa các biến môi trường. API key của Google được lưu ở đây.
+- `requirements.txt`: Danh sách các thư viện cần thiết cho dự án.
+- `README.md`: File hướng dẫn cài đặt và sử dụng.
+
+## Yêu cầu
+
+- streamlit
+- google-generativeai
+- python-dotenv
+- langchain
+- PyPDF2
+- chromadb
+- faiss-cpu
+- langchain_google_genai
+- langchain_community
+- langdetect
+
+## Tác giả
+
+- [Google Gemini](https://ai.google.com/): Mô hình AI trò chuyện được đào tạo trên tài liệu PDF.
+- [Streamlit](https://streamlit.io/): Thư viện Python cho việc xây dựng ứng dụng web dữ liệu.
---
+++ app.py
@@ -0,0 +1,164 @@
+import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import streamlit as st
+import google.generativeai as genai
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+from langdetect import detect
+
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# read all pdf files and return text
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text() if page.extract_text() else ""
+    return text
+
+# split text into chunks
+
+
+def get_text_chunks(text):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=10000, chunk_overlap=1000)
+    chunks = splitter.split_text(text)
+    return chunks  # list of strings
+
+# get embeddings for each chunk
+
+
+def get_vector_store(chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
+def get_conversational_chain():
+    prompt_template = """
+    Bạn là một trợ lý ảo thông minh, được thiết kế để cung cấp câu trả lời về các vấn đề dựa trên các tài liệu tham khảo đã cho. Vui lòng làm theo các hướng dẫn sau:
+    - Đọc và phân tích tài liệu tham khảo.
+    - Sử dụng thông tin từ tài liệu để trả lời câu hỏi đã cho.
+    - Nếu không tìm thấy đủ thông tin trong tài liệu, hãy báo rằng "không có thông tin đủ" và gợi ý cho người dùng thêm thông tin vào câu hỏi để rõ ràng hơn.
+    - Trả lời với các số liệu, thời gian, hoặc đường dẫn đến nội dung có trong tài liệu nếu có.
+    - Trả lời câu hỏi một cách ngắn gọn và tự nhiên nhất.
+
+    Ngữ cảnh:
+    {context}
+
+    Câu hỏi:
+    {question}
+
+    Trả lời:
+    """
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest",
+                                   client=genai,
+                                   temperature=0.9)
+    prompt = PromptTemplate(template=prompt_template,
+                            input_variables=["context", "question"])
+    chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
+    return chain
+
+
+def clear_chat_history():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Upload some PDFs and ask me a question"}]
+    st.session_state.context = ""
+
+
+def user_input(user_question):
+    detected_language = detect(user_question)
+    language = "English" if detected_language == 'en' else "Vietnamese"
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    if not os.path.exists("faiss_index"):
+        raise FileNotFoundError(
+            "The FAISS index file does not exist. Please process some PDFs first.")
+    new_db = FAISS.load_local(
+        "faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversational_chain()
+
+    # Append current question to the context
+    context = st.session_state.context
+    if context:
+        context += "\n" + user_question
+    else:
+        context = user_question
+
+    response = chain({"input_documents": docs, "context": context,
+                     "question": user_question}, return_only_outputs=True)
+
+    # Update the session state context
+    st.session_state.context = context
+
+    return response
+
+
+def main():
+    st.set_page_config(page_title="Gemini PDF Chatbot", page_icon="🤖")
+    if "context" not in st.session_state:
+        st.session_state.context = ""
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader(
+            "Tải lên các tài liệu PDF và nhấn nút Submit & Process", accept_multiple_files=True)
+
+        if not pdf_docs:
+            st.warning("Vui lòng tải lên các tài liệu PDF")
+        else:
+            # Kiểm tra xem người dùng đã nhấn nút Submit & Process chưa, nếu nhấn rồi thì sẽ thực hiện các bước sau
+            if st.button("Submit & Process"):
+                with st.spinner("Processing..."):
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    get_vector_store(text_chunks)
+                    st.success("Done")
+                    st.balloons() # Hiển thị biểu tượng bóng bay   
+
+    st.title("Chat với tài liệu PDF sử dụng Gemini")
+    st.write("Hãy tải lên các tài liệu PDF và đặt câu hỏi cho trợ lý ảo")
+
+    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+    
+    # Hiển thị  By: Trương Văn Thông với chữ gradient
+    st.sidebar.markdown(
+        '<p style="color:#f63366; font-size: 24px">Web By: Trương Văn Thông</p>',
+        unsafe_allow_html=True
+    )
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Upload some PDFs and ask me a question"}]
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = user_input(prompt)
+                placeholder = st.empty()
+                full_response = ''
+                for item in response['output_text']:
+                    full_response += item
+                    placeholder.markdown(full_response)
+                placeholder.markdown(full_response)
+        if response is not None:
+            message = {"role": "assistant", "content": full_response}
+            st.session_state.messages.append(message)
+
+
+if __name__ == "__main__":
+    main()
